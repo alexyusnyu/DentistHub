18663f526cd951e1caeb94a494236f629b0d87ab
Alex <slayer1649@gmail.com>
2024-03-25 14:03:20 +0200

    url fix

--- DentistBook/common/urls.py
+++ DentistBook/common/urls.py
@@ -3,7 +3,7 @@ from DentistBook.common.views import home_page, map_page, search_view, about_pag
 
 urlpatterns = [
     path('', home_page, name='home-page'),
-    path('map-all-dentistsoffice', map_page, name='map-page'),
+    path('map-all-dentistsoffices', map_page, name='map-page'),
     path('search/', search_view, name='search-view'),
     path('about/', about_page, name='about-page')
 ]
--- DentistBook/dentistsoffice/apps.py
+++ DentistBook/dentistsoffice/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DentistsofficeConfig(AppConfig):
+class DentistsofficesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'DentistBook.dentistsoffice'

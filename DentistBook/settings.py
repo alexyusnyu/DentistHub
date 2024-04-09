1877d8129ee864cb7945a1f3a4738237c38b411e
Alex <slayer1649@gmail.com>
2024-04-10 00:01:23 +0300

    x

--- DentistBook/settings.py
+++ DentistBook/settings.py
@@ -99,7 +99,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = 'BASE_DIR/static/'
+STATIC_ROOT = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media_files'
